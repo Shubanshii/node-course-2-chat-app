41aac6e16b1a0ef53ae1a2138ee35b1477beeed0
unknown <c_day@alumni.baylor.edu>
2018-07-08 17:35:53 -0500

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 let socket = io();
 
+function scrollToBottom () {
+  let messages = jQuery('#messages');
+  let newMessage = messages.children('li:last-child');
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 
@@ -19,6 +33,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -29,8 +44,9 @@ socket.on('newLocationMessage', function (message) {
     from: message.from,
     createdAt: formattedTime
   });
-  
+
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
